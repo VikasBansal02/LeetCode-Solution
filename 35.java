4ab0a1ae2d9854599feb2dac0c5ab39363343f73
VikasBansal02 <97593886+VikasBansal02@users.noreply.github.com>
2022-11-12 13:08:07 +0530

    Search Insert Position
    35 LeetCode

---
+++ 35.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length-1;
+        int ans=0;
+        while(lo<=hi){
+            int mid=lo + (hi-lo)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                lo = mid+1;
+            }
+            else{
+                hi = mid-1;
+            }
+        }
+        // if(nums[hi]<target)
+        //     return lo;
+        return lo;
+        
+    }
+}
