3040709d1785ffd84829389fd88f5bb5f2a21b2b
VikasBansal02 <97593886+VikasBansal02@users.noreply.github.com>
2022-11-12 13:09:48 +0530

    374. Guess Number Higher or Lower
    374 LeetCode

---
+++ 374.java
@@ -0,0 +1,19 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int lo = 1;
+        int hi = n;
+        // int ans=0;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(guess(mid)==0){
+                return mid;
+            }
+            else if(guess(mid)==-1){
+                hi=mid-1;
+            }
+            else
+                lo=mid+1;
+        }
+        return 0;
+    }
+}
