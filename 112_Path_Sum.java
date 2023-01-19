31cabc8994c006fa9f2cc55d19cb12f622e12b35
Vikas Bansal <97593886+VikasBansal02@users.noreply.github.com>
2023-01-19 09:14:16 +0530

    112. Path Sum

---
+++ 112_Path_Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+			return false;
+		}
+
+		if (root.left == null && root.right == null) {// leafNode Condition
+			return targetSum - root.val == 0;
+		}
+		boolean left = hasPathSum(root.left, targetSum - root.val);
+		boolean right = hasPathSum(root.right, targetSum - root.val);
+		return left || right;
+    }
+}
